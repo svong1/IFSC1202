86203b438957221ceef4fd66b420aea861d6fabb
svong1 <142817902+svong1@users.noreply.github.com>
2023-08-25 01:08:49 +0000

    Python Introduction

---
+++ 01.05 Previous and Next.py
@@ -0,0 +1,8 @@
+Number = input("Enter Number: ")
+print("Enter Number" + str(Number))
+intNumber = int(Number)
+previous = intNumber - 1
+nextNumber = intNumber + 1
+
+print("The previous number is " + str(previous) + ".")
+print("The next number is " + str(nextNumber) + ".")
