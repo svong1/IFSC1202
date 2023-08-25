439f850713ce0123da8c21bb2ddbe330603a24f6
svong1 <142817902+svong1@users.noreply.github.com>
2023-08-25 00:20:52 +0000

    Python Introduction

---
+++ 01.03 Square.py
@@ -0,0 +1,16 @@
+# x ** y 
+# print (x)
+# print(type(x))
+
+# x = 5
+# print(x)
+# then, 
+# x **=2    exponent
+# = 25
+
+Number = input("Please type your number to caculate the square of the number: ")
+print("You entered: " + Number)
+intNumber = int(Number)
+x = intNumber * intNumber
+
+print(x)
